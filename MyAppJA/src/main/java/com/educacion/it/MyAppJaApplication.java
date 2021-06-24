0d420c074516d55a9b8e6fc5f03b3bc2175d72aa
Rafael Ibarra <rafael.dante@gmail.com>
2021-06-23 22:03:26 -0300

    primer commit

---
+++ MyAppJA/src/main/java/com/educacion/it/MyAppJaApplication.java
@@ -0,0 +1,13 @@
+package com.educacion.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppJaApplication.class, args);
+	}
+
+}
---
+++ MyAppJA/src/test/java/com/educacion/it/MyAppJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educacion.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
