3800a977de254b5d01dcb680b2c3976e40b5c32a
Rafael Ibarra <rafael.dante@gmail.com>
2021-06-28 21:03:49 -0300

    se agrega clase persona

---
+++ MyAppJA/src/main/java/com/educacion/it/components/Persona.java
@@ -0,0 +1,23 @@
+package com.educacion.it.components;
+
+public class Persona {
+
+    String nombre;
+    String apellido;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
